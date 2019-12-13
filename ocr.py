78d9badac673bfed4f243c03f74a803123f5933f
kriteimk <kriteimk@gmail.com>
2019-12-13 16:31:28 +0300

    Included tesseract library right into project

--- ocr.py
+++ ocr.py
@@ -3,7 +3,7 @@
 def detectText(path):
     import pytesseract
     from PIL import Image
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = './utils/Tesseract-OCR/tesseract.exe'
     im = Image.open(path)
     im = im.convert('L')
     im = im.point(lambda x: 0 if x < 170 else 255)
